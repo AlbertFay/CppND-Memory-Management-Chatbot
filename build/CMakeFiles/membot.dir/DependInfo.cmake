ecf867270d91dae3628ee9999535452b8f27f92a
albertfay1121 <albertfay1121@gmail.com>
2021-01-21 16:02:46 -0800

    Edit

---
+++ build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/src/chatbot.cpp" "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatbot.cpp.obj"
+  "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/src/chatgui.cpp" "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatgui.cpp.obj"
+  "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/src/chatlogic.cpp" "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/chatlogic.cpp.obj"
+  "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/src/graphedge.cpp" "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/graphedge.cpp.obj"
+  "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/src/graphnode.cpp" "C:/Users/Albert/OneDrive/Documents/C++ Projects/CppND-Memory-Management-Chatbot/build/CMakeFiles/membot.dir/src/graphnode.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNICODE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/C++ Development/Libs/wxWidgets-3.1.4/lib/gcc_lib/mswu"
+  "C:/C++ Development/Libs/wxWidgets-3.1.4/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
